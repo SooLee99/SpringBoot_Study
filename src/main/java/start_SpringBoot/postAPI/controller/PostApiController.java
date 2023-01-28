708b265574f1df075c5da9196b918659df69cba1
LeeSooo <dltn990930@naver.com>
2023-01-28 17:21:09 +0900

    chore : PostAPI 주석(설명) (2023-01-28)

--- src/main/java/start_SpringBoot/postAPI/controller/PostApiController.java
+++ src/main/java/start_SpringBoot/postAPI/controller/PostApiController.java
@@ -1,17 +1,43 @@
 package start_SpringBoot.postAPI.controller;
 
+import org.springframework.web.bind.annotation.*;
 import start_SpringBoot.postAPI.dto.PostRequestDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class PostApiController {
 
-    @GetMapping("/post")
-    public void post(@RequestBody PostRequestDto requestData){
+    @PostMapping("/post1")
+    // @PostMapping : 주어진 URI 표현식과 일치하는 HTTP POST 요청을 처리합니다.
+
+    // ▶ HTTP POST?
+    //  - HTTP POST 메서드는 서버로 데이터를 전송한다.
+    //  - POST 요청은 보통 "HTML 양식을 통해 서버에 전송"하며, 서버에 변경사항을 만든다.
+
+    // ▶ 주요 기능 (추가/등록)
+    //  - 기존 리소스에 주석달기
+    //  - 게시판, 뉴스, 그룹, 메일링 리스트나 이와 유사한 시스템에 글 올리기
+    //  - 회원가입 모달로 새로운 사용자 추가하기
+    //  - 양식 전송 결과 등 데이터 블록을 데이터 처리 프로세스에 보내기
+    //  - 이어붙이기 연산을 통한 데이터베이스 확장
+
+    public void post1(@RequestBody Map<String, Object> requestData) {
+        requestData.forEach((key, value) -> {
+            System.out.println("key : " + key);
+            System.out.println("key : " + value);
+        });
+    }
+
+    @PostMapping("/post2")
+    public void post2(@RequestBody PostRequestDto requestData) {
         System.out.println(requestData);
+
+        requestData.getAccount();
+        requestData.getEmail();
+        requestData.getPassword();
+        requestData.getAddress();
+        requestData.getPhoneNumber();
     }
 }
--- src/main/java/start_SpringBoot/postAPI/dto/PostRequestDto.java
+++ src/main/java/start_SpringBoot/postAPI/dto/PostRequestDto.java
@@ -2,12 +2,15 @@ package start_SpringBoot.postAPI.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+// 서버단에서는 카멜 케이스(Camel Case) 방식을 사용. - 첫 글자는 소문자로, 중간 글자들은 대문자로 시작하는 표기법 ex) phoneNumber
+// 클라이언트단에서는 스네이크 케이스(Snake Case) 방식을 사용. - 언더바(_)가 포함된 표기법. ex) phone_number
 public class PostRequestDto {
     private String account;
     private String email;
     private String address;
     private String password;
     @JsonProperty("phone_number")
+    //  @JsonProperty : JSON 직렬화 시, 설정할 수 있는 이름을 지정하는 어노테이션이다.
     private String phoneNumber;
 
     @JsonProperty("OTP")
