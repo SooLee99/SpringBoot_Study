7b14924c58b28cb9e8d5923a231435fabfaf1dea
LeeSooo <dltn990930@naver.com>
2023-01-28 17:32:28 +0900

    chore : PostAPI 주석(설명) (2023-01-28)

--- src/main/java/start_SpringBoot/putAPI/dto/PostRequestDto.java
+++ src/main/java/start_SpringBoot/putAPI/dto/PostRequestDto.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import java.util.List;
 
 @JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
+// @JsonNaming : 전체 멤버변수를 새로 네이밍 해준다.(내부적으로 보면 대문자가 나오는 자리에 _와 소문자로 바꿔 넣어주는 간단한 구조를 가지고 있다.)
 public class PostRequestDto {
     private String name;
     private String age;
