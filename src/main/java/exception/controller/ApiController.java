e102b6b3f13bdc91fb1a528693300141ec0ab954
LeeSooo <dltn990930@naver.com>
2023-02-08 20:32:52 +0900

    feat : Exception 예제정리 (2023-02-08)

---
+++ src/main/java/exception/controller/ApiController.java
@@ -0,0 +1,37 @@
+package exception.controller;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import validation.dto.User;
+
+@RestController
+@RequestMapping("/api/user")
+@Validated
+public class ApiController {
+
+    @GetMapping("") // ?name=1234
+    public User get(
+            @Size(min = 2)
+            @RequestParam String name,
+
+            @NotNull
+            @Min(1)
+            @RequestParam Integer age){
+        User user = new User();
+        user.setName(name);
+        user.setAge(age);
+
+        return user;
+    }
+
+
+    @PostMapping("")
+    public User post(@Valid @RequestBody User user){
+        System.out.println(user);
+        return user;
+    }
+
+}
